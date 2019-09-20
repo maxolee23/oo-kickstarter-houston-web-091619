7fe566cacc057e9bb001ce3e575164e29955050c
Max Lee <maxolee23@gmail.com>
2019-09-20 19:18:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,9 +2,14 @@ class Backer
 
   attr_reader :name
 
+  @@backed_projects = []
+
   def initialize(name)
     @name = name
-    @backed_projects = []
+  end
+
+  def backed_projects
+    @@backed_projects
   end
 
 
